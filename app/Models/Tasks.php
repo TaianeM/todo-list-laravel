911c562ea3024602676e4c067da112e9553a4256
TaianeM <taianemedeiro16@gmail.com>
2025-08-09 11:55:17 -0400

    feat: Tasks | create model, migrate e enum

---
+++ app/Models/Tasks.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tasks extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'due_date'
+    ];
+}
